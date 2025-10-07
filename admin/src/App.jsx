2414381a0a1870ae41e94918ce16e432d0d7568f
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-10-08 01:01:43 +0500

    added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,9 @@ import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorList from './pages/Admin/DoctorList';
 import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointments from './pages/Doctor/DoctorAppointments';
+import DoctorProfile from './pages/Doctor/DoctorProfile'
 
 const App = () => {
 
@@ -25,11 +28,19 @@ const App = () => {
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
+
+          {/* Admin routes */}
           <Route path='/' element = {<></>} />
           <Route path='/admin-dashboard' element = {<Dashboard/>} />
           <Route path='/all-appointments' element = {<AllAppointments/>} />
           <Route path='/add-doctor' element = {<AddDoctor/>} />
           <Route path='/doctor-list' element = {<DoctorList/>} />
+
+          {/* Doctor Routes */}
+          <Route path='/doctor-dashboard' element = {<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element = {<DoctorAppointments />} />
+          <Route path='/doctor-profile' element = {<DoctorProfile />} />
+
         </Routes>
       </div>
 
--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
 
 const DoctorAppointments = () => {
+
+  const { dToken, appointments, getAppointments } = useContext(DoctorContext)
+
+  useEffect(()=> { 
+    if(dToken){
+      getAppointments();
+    }
+  },[dToken])
+
   return (
     <div>
       
+    <p>All Appointments</p>
+
+    <div>
+
+      <div>
+        <p>#</p>
+        <p>Patient</p>
+        <p>Payemnt</p>
+        <p>Age</p>
+        <p>Date & Time</p>
+        <p>Fees</p>
+        <p>Action</p>
+      </div>
+
+    </div>
+
     </div>
   )
 }
